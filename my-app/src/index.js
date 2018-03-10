1fa86e3c5884d7e983a7ed6f587f579032d0054e
arashabedin <arash.abedin@gmail.com>
2018-03-10 21:49:42 +0100

    Multiple fetching

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App>hello world</App> , document.getElementById('root'));
+ReactDOM.render(<App>The members</App> , document.getElementById('root'));
 registerServiceWorker();
