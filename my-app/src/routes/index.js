9017c8105270496b4f2816def890553b9af6a906
arashabedin <arash.abedin@gmail.com>
2018-03-11 12:48:04 +0100

    route redirection

--- my-app/src/routes/index.js
+++ my-app/src/routes/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import{BrowserRouter,Route} from 'react-router-dom';
+import{BrowserRouter,Route,Redirect} from 'react-router-dom';
 import Home from './components/Home';
 import User from './components/User';
 
 export default() => (
 <BrowserRouter>
 <div>
+<Route exact path="/" render={() => (
+    <Redirect to="/home"/>
+)}/>
 <Route path="/home" component={Home}/>
 <Route path="/user/:username" component={User}/>
 
